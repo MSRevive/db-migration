ea18a311ced9557be4a5cc7937e57b5c001f8b98
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2024-08-18 14:48:05 -0500

    rewrite database stuff for migration

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 	_ "modernc.org/sqlite"
 	"github.com/spf13/pflag"
 	"github.com/msrevive/nexus2/pkg/database/schema"
+	"github.com/msrevive/nexus2/pkg/database/bsoncoder"
 	"github.com/google/uuid"
+	"go.etcd.io/bbolt"
 )
 
 type flags struct {
@@ -97,7 +99,17 @@ func main() {
 			panic(fmt.Sprintf("failed to rename original DB file: %v", err))
 		}
 
-		fmt.Println("Created original database backup.")
+		fmt.Println("Opening connection to new database")
+		newDB, err := bbolt.Open(flags.destDBFile, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer newDB.Close()
+
+		fmt.Println("Creating buckets for new database")
+		if err := createBucket(newDB); err != nil {
+			panic(err)
+		}
 
 		// Open SQLite connection
 		fmt.Println("Opening SQLite file")
@@ -121,43 +133,90 @@ func main() {
 				panic(err)
 			}
 
-			var oldChar oldChar
-			charRow := db.QueryRow("SELECT id, created_at, slot, size, data FROM characters WHERE player_id = ? AND version = ? LIMIT 1", oldPlayer.ID, 1)
-			if err := charRow.Scan(&oldChar.ID, &oldChar.CreatedAt, &oldChar.Slot, &oldChar.Size, &oldChar.Data); err != nil {
-				panic(err)
-			}
-
+			// we create the new user structure here so character's can be filled in when we get them.
 			newUser := schema.User{
 				ID: oldPlayer.SteamID,
 				Characters: make(map[int]uuid.UUID),
 				DeletedCharacters: make(map[int]uuid.UUID),
 			}
-			newUser.Characters[oldChar.Slot] = oldChar.ID
 
-			// newCharData := schema.CharacterData {
-			// 	CreatedAt: oldChar.CreatedAt,
-			// 	Size: oldChar.Size,
-			// 	Data: oldChar.Data,
-			// }
-			newChar := schema.Character{
-				ID: oldChar.ID,
-				SteamID: oldPlayer.SteamID,
-				Slot: oldChar.Slot,
-				CreatedAt: oldChar.CreatedAt,
-				Data: schema.CharacterData {
+			var oldCharSlot1 oldChar
+			charRowSlot1 := db.QueryRow("SELECT id, created_at, size, data FROM characters WHERE player_id = ? AND version = 1 AND slot = ?", oldPlayer.ID, 0)
+			if err := charRowSlot1.Err(); err == nil {
+				charRow.Scan(&oldCharSlot1.ID, &oldCharSlot1.CreatedAt, &oldCharSlot1.Size, &oldCharSlot1.Data)
+
+				newUser.Characters[1] = oldChar.ID
+
+				newChar := schema.Character{
+					ID: oldChar.ID,
+					SteamID: oldPlayer.SteamID,
+					Slot: 1,
 					CreatedAt: oldChar.CreatedAt,
-					Size: oldChar.Size,
-					Data: oldChar.Data,
-				},
+					Data: schema.CharacterData {
+						CreatedAt: oldChar.CreatedAt,
+						Size: oldChar.Size,
+						Data: oldChar.Data,
+					},
+				}
+
+				fmt.Printf("Importing character slot %d", newChar.Slot)
+				if err := insertChar(newDB, newChar); err != nil {
+					panic(err)
+				}
+			}
+
+			var oldCharSlot2 oldChar
+			charRowSlot2 := db.QueryRow("SELECT id, created_at, size, data FROM characters WHERE player_id = ? AND version = 1 AND slot = ?", oldPlayer.ID, 1)
+			if err := charRowSlot2.Err(); err == nil {
+				charRow.Scan(&oldCharSlot2.ID, &oldCharSlot2.CreatedAt, &oldCharSlot2.Size, &oldCharSlot2.Data)
+
+				newUser.Characters[2] = oldChar.ID
+
+				newChar := schema.Character{
+					ID: oldChar.ID,
+					SteamID: oldPlayer.SteamID,
+					Slot: 2,
+					CreatedAt: oldChar.CreatedAt,
+					Data: schema.CharacterData {
+						CreatedAt: oldChar.CreatedAt,
+						Size: oldChar.Size,
+						Data: oldChar.Data,
+					},
+				}
+
+				fmt.Printf("Importing character slot %d", newChar.Slot)
+				if err := insertChar(newDB, newChar); err != nil {
+					panic(err)
+				}
+			}
+
+			var oldCharSlot3 oldChar
+			charRowSlot3 := db.QueryRow("SELECT id, created_at, size, data FROM characters WHERE player_id = ? AND version = 1 AND slot = ?", oldPlayer.ID, 2)
+			if err := charRowSlot3.Err(); err == nil {
+				charRow.Scan(&oldCharSlot3.ID, &oldCharSlot3.CreatedAt, &oldCharSlot3.Size, &oldCharSlot3.Data)
+
+				newUser.Characters[3] = oldChar.ID
+
+				newChar := schema.Character{
+					ID: oldChar.ID,
+					SteamID: oldPlayer.SteamID,
+					Slot: 3,
+					CreatedAt: oldChar.CreatedAt,
+					Data: schema.CharacterData {
+						CreatedAt: oldChar.CreatedAt,
+						Size: oldChar.Size,
+						Data: oldChar.Data,
+					},
+				}
+
+				fmt.Printf("Importing character slot %d", newChar.Slot)
+				if err := insertChar(newDB, newChar); err != nil {
+					panic(err)
+				}
 			}
 
 			fmt.Printf("Importing user %s", newUser.ID)
-			if err := insertUser(newUser); err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("Importing character slot %d", newChar.Slot)
-			if err := insertChar(newChar); err != nil {
+			if err := insertUser(newDB, newUser); err != nil {
 				panic(err)
 			}
 		}
@@ -166,10 +225,29 @@ func main() {
 	}
 }
 
-func insertUser(user schema.User) error {
+func createBucket(db *bbolt.DB) error {
+	if err := db.Update(func(tx *bbolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists([]byte("users"))
+		if err != nil {
+			return fmt.Errorf("failed to create users bucket: %s", err)
+		}
+
+		_, err = tx.CreateBucketIfNotExists([]byte("characters"))
+		if err != nil {
+			return fmt.Errorf("failed to create characters bucket: %s", err)
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+} 
+
+func insertUser(db *bbolt.DB, user schema.User) error {
 	return nil
 }
 
-func insertChar(char schema.Character) error {
+func insertChar(db *bbolt.DB, char schema.Character) error {
 	return nil
 }
